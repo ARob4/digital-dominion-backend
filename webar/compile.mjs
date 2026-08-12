b047c62c9d2ed9a4cb80a03694b4c485b97397c1
ARob4 <angelrobles@arcreativemarketing.com>
2026-08-12 12:15:15 -0400

    Compile target from normalized PNG

--- webar/compile.mjs
+++ webar/compile.mjs
@@ -2,7 +2,7 @@ import { OfflineCompiler } from 'mind-ar/src/image-target/offline-compiler.js';
 import { writeFile } from 'fs/promises';
 import { loadImage } from 'canvas';
 
-const image = await loadImage('webar/target.jpg');
+const image = await loadImage('webar/target.png');
 const compiler = new OfflineCompiler();
 await compiler.compileImageTargets([image], (percent) => {
   process.stdout.write(`\rCompiling image target: ${Math.round(percent)}%`);
